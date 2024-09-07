b398ccf7fd07a9e69ec3bd3104bcd94c9195a07a
Gabriel Lacerda Araujo <161892266+GLacerda1@users.noreply.github.com>
2024-09-07 09:22:11 -0300

    Ajustes no GlobalStyle.jsx

--- src/css/GlobalStyle.jsx
+++ src/css/GlobalStyle.jsx
@@ -9,35 +9,21 @@ const GlobalStyle = createGlobalStyle`
     text-decoration:none;
 }
 
-:root {
+    :root {
     --color-padrao1: #864AAD;
     --color-padrao2: #450D69;
     --color-padrao3: #67149C;
     --color-padrao4: #9A1EE9;
     --color-padrao5: #B564E9;
-} 
+}
+
+    html, body {
+    height: 100%;
+    margin: 0;
+}
 
 html{
     scroll-behavior:smooth;
-}
-
-body{
-
-    display:grid;
-    grid-template-areas:
-        'nav nav nav nav'
-        'home home home home'
-        'sobre sobre sobre sobre'
-        'carros carros carros carros'
-        'login login login login'
-
-    grid-template-columns:repeat(5,0.5fr);
-    grid-template-rows:repeat(5);
-    font-size:1rem; 
-}
-
-.fimtech{
-    background-color: var(--color-padrao)
 }
 
 `;
