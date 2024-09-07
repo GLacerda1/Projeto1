074d2b3de8aeaffa8c4df7acb09953252577478d
Melyssa Huang Gonçalves <161510026+Melmis@users.noreply.github.com>
2024-09-06 22:20:54 -0300

    Mudando ProdutosStyle.jsx

--- src/css/ProdutosStyle.jsx
+++ src/css/ProdutosStyle.jsx
@@ -1,17 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const ProdutosStyle =styled.section`
+export const ProdutosStyle = styled.section`
+  .produtos {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 20px;
+    padding: 20px;
+    justify-items: center;
+    align-items: start;
+    background-size: cover;
+  }
 
+  .card {
+    border: 1px solid;
+    border-radius: 10px;
+    padding: 20px;
+    text-align: center;
+    background-color: #fff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transition: transform 0.2s;
+  }
 
-.produtos{
-    grid-area:home;
-    height:62.35vh;
-    display:flex;
-    background-size:cover;
-    justify-content:center;
-    align-items:center;
-}
+  .card:hover {
+    transform: scale(1.05);
+  }
 
+  .card .card-image {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
 
-
-`
+  img{
+    width:100px;
+    height: 100px;
+  }
+`;
