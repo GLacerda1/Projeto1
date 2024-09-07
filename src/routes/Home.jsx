bf604fd5cd1cb82d1bfbad0e733f0177741a573e
Gabriel Lacerda Araujo <161892266+GLacerda1@users.noreply.github.com>
2024-09-07 20:43:24 -0300

    Atualização Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,21 @@
-import { HomeStyle } from '../css/HomeStyle'
+import { HomeStyle } from '../css/HomeStyle';
 
-const Home=()=>{
-    return(
-        <>
+const Home = () => {
+    return (
         <HomeStyle>
             <section className="home">
-                <h1>DIRIJA COM EMOÇÃO...</h1>
+                <div className="main">
+                    <h1>Bem-vindo ao Mundo dos Esportes!</h1>
+                    <p>Explore nossa seleção para todos os esportes e encontre o que você precisa.</p>
+                </div>
+                <div className="info">
+                    <h2>Sobre Nós</h2>
+                    <p>Na nossa loja, acreditamos que o esporte é para todos. Oferecemos uma ampla gama de equipamentos e acessórios para todos os tipos de esportes, com um atendimento amigável e especializado.</p>
+                    <p>Nosso objetivo é ajudar você a alcançar suas metas esportivas e a se divertir ao máximo!</p>
+                </div>
             </section>
         </HomeStyle>
-        </>
-    )
+    );
 }
 
-export default Home
+export default Home;
