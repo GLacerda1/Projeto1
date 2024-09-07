66585a0537726ebfdfa60bafa95bf85a9a9ffa6f
Gabriel Lacerda Araujo <161892266+GLacerda1@users.noreply.github.com>
2024-09-06 21:30:04 -0300

    Mudando NavStyle.jsx

--- src/css/NavStyle.jsx
+++ src/css/NavStyle.jsx
@@ -1,41 +1,53 @@
 import styled from "styled-components";
 
-export const NavStyle =styled.section`
+export const NavStyle = styled.section`
+    .navegacao {
+        display: flex;
+        gap: 20px;
+        float: right;
+        margin-right: 30px;
+        margin-top: 12px;
+        justify-content: flex-start;
+        align-items: center;
+    }
 
+    .navegacao ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        gap: 20px;
+    }
 
-.navegacao {
-    display: flex;
-    gap: 10px;
-    float: right;
-    margin-right: 30px;
-    margin-top: 12px;
-}
-.navegacao-link {
-    color: var(--color-padrao5);
-    text-decoration: none;
-    background-color: var(--color-padrao3); 
-    border: none;
-    border-radius: 8px; 
-    padding: 10px 20px;
-    font-size: 16px; 
-    font-weight: bold;
-    cursor: pointer; 
-    transition: background-color 0.3s ease, transform 0.3s ease;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
-}
+    .navegacao-link, .navegacao-button {
+        color: var(--color-padrao5);
+        text-decoration: none;
+        background-color: var(--color-padrao3); 
+        border: none;
+        border-radius: 8px; 
+        padding: 10px 20px;
+        font-size: 16px; 
+        font-weight: bold;
+        cursor: pointer; 
+        transition: background-color 0.3s ease, transform 0.3s ease;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+    }
 
-.navegacao-link:hover {
-    background-color: var(--color-padrao4); 
-    transform: translateY(-2px); 
-}
+    .navegacao-link:hover, .navegacao-button:hover {
+        background-color: var(--color-padrao4); 
+        transform: translateY(-2px); 
+    }
 
-.navegacao-link:active {
-    background-color: var(--color-padrao5); 
-    transform: translateY(1px); 
-}
+    .navegacao-link:active, .navegacao-button:active {
+        background-color: var(--color-padrao5); 
+        transform: translateY(1px); 
+    }
 
-.navegacao-link:focus {
-    outline: none; 
-}
+    .navegacao-link:focus, .navegacao-button:focus {
+        outline: none; 
+    }
 
-`
+    .navegacao-button + .navegacao-button {
+        margin-left: 15px;
+    }
+`;
