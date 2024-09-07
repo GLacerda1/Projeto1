24ce310a1cab601a7dc76c0a5b1a45def30f2ca9
Gabriel Lacerda Araujo <161892266+GLacerda1@users.noreply.github.com>
2024-09-07 09:29:06 -0300

    Ajustando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import Nav from './components/Nav'
-import Footer from './components/Footer'
-import {Outlet} from 'react-router-dom'
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 import GlobalStyle from './css/GlobalStyle';
+import styled from 'styled-components';
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.main`
+  flex: 1;
+`;
 
 function App() {
-
-
   return (
-    <>
-      <GlobalStyle/>
-      <Nav/>
-      <Outlet/>
-      <Footer/>
-    </>
-  )
+    <PageWrapper>
+      <GlobalStyle />
+      <Nav />
+      <Content>
+        <Outlet />
+      </Content>
+      <Footer />
+    </PageWrapper>
+  );
 }
 
-export default App
+export default App;
