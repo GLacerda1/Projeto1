c42bade08766dbf0afc3de4f265e914254d815e4
Gabriel Lacerda Araujo <161892266+GLacerda1@users.noreply.github.com>
2024-09-07 20:45:20 -0300

    Atualização Sobre.jsx

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,5 +1,5 @@
 import { SobreStyle } from '../css/SobreStyle';
-import fimtech from '../assets/fimtech.png'
+import fimtech from '../assets/fimtech.png';
 
 const Sobre = () => {
     return (
@@ -7,11 +7,9 @@ const Sobre = () => {
             <section className='sobre'>
                 <div>
                     <img src={fimtech} alt="fimtech" />
-                    <p>Lorem ipsum dolor sit amet 
-                        consectetur adipisicing elit. 
-                        Eum vitae neque quidem deserunt necessitatibus 
-                        ad sapiente impedit nulla, fuga dolores molestias 
-                        dolorum repudiandae consequatur, sint ab consequuntur praesentium suscipit provident!
+                    <p>
+                        Musculação: Sua Jornada de Força com a Fimtech
+                        Na Fimtech, acreditamos que a musculação é mais do que um treino — é uma jornada de autodescoberta e fortalecimento, tanto do corpo quanto da mente. Queremos ser seus parceiros nessa caminhada, oferecendo tudo o que você precisa para alcançar seus objetivos, sejam eles ganho de força, definição muscular, ou simplesmente sentir-se bem consigo mesmo. Nossa loja é o destino certo para quem busca qualidade e variedade em equipamentos de musculação. Desde halteres ajustáveis e kettlebells até estações de treino multifuncionais, temos uma ampla gama de produtos projetados para atender todas as suas necessidades, seja para um treino em casa ou na academia. Além disso, contamos com uma equipe de especialistas sempre pronta para ajudar você a escolher o equipamento ideal, de acordo com o seu perfil e suas metas. Na Fimtech, também sabemos que o esporte é para todos, independentemente do nível de experiência ou da modalidade praticada. Por isso, nossos produtos são pensados para garantir conforto, segurança e performance, do iniciante ao atleta avançado. Acreditamos que o esporte é uma ferramenta poderosa para transformar vidas, e queremos fazer parte da sua transformação. Venha conhecer nossa linha completa de equipamentos e acessórios para musculação, e descubra como a Fimtech pode ajudar você a atingir seu próximo nível.
                     </p>
                 </div>
             </section>
@@ -19,4 +17,4 @@ const Sobre = () => {
     );
 }
 
-export default Sobre; 
+export default Sobre;
